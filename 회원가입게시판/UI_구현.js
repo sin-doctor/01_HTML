5779ae56a703031a28f30e986ebbb8c5baafe2aa
sin-doctor <sbs1602@naver.com>
2024-11-12 12:00:49 +0900

    함수 매개변수 전달인자개요

---
+++ 회원가입게시판/UI_구현.js
@@ -0,0 +1,12 @@
+function showhint(){
+    const hint = document.getElementById("hint");
+    hint.style.display = "block";
+}
+function blindhint(){
+    const hint = document.getElementById("hint");
+    hint.style.display = "none";
+}
+function showhint1(){
+    const hint1 = document.getElementById("hint1");
+    hint1.style.display = "block";
+}
